1d49792f0df447063ed0562bb263db44b1d39c01
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-02-24 16:26:20 +0530

    Merged 'main'

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 
 <div class="h-screen relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-    <img src="header.jpg" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center">
+    <img src="header.jpg" alt="header" class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center">
     <svg viewBox="0 0 1097 845" aria-hidden="true" class="hidden transform-gpu blur-3xl sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:w-[68.5625rem]">
       <path fill="url(#10724532-9d81-43d2-bb94-866e98dd6e42)" fill-opacity=".2" d="M301.174 646.641 193.541 844.786 0 546.172l301.174 100.469 193.845-356.855c1.241 164.891 42.802 431.935 199.124 180.978 195.402-313.696 143.295-588.18 284.729-419.266 113.148 135.13 124.068 367.989 115.378 467.527L811.753 372.553l20.102 451.119-530.681-177.031Z" />
       <defs>
