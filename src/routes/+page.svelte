2ad40cbb9941690f0d5e11dc44eeea20b53b41c2
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-02-24 16:37:08 +0530

    Pushed 'main'

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,14 +35,21 @@
                     SIGN UP
                 </button>
             </a>
-            <span>
+            <span class="pl-3">
                 <a href="/signIn">SIGN IN</a>
             </span>
+            <div>
+                <button type="submit" class="flex-none rounded-md bg-cyan-500 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-500">Search</button>
+            </div>
         </div>
       </div>
     </div>
   </div>
 
+
+
+   
+ 
 
 <style>
 
