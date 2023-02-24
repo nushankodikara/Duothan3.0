61a55d64244aba98ce5f78a4d8ab110f08e9f625
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-02-24 15:24:14 +0530

    Merged 'main'

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,39 +2,43 @@
 
 </script>
 
-<div class="h-screen bg-auto bg-no-repeat bg-center " style="background-image: url('/header.jpg')">
-    <div class="h-4/5 w-screen">
-        <h1 class="inline-block align-top text-5xl text-center text-black">Welcome to </h1>        
-        <h2 class="inline-block align-top text-2xl text-center text-black">Slogan</h2>
+<div class="h-screen relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+    <img src="header.jpg" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center">
+    <svg viewBox="0 0 1097 845" aria-hidden="true" class="hidden transform-gpu blur-3xl sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:w-[68.5625rem]">
+      <path fill="url(#10724532-9d81-43d2-bb94-866e98dd6e42)" fill-opacity=".2" d="M301.174 646.641 193.541 844.786 0 546.172l301.174 100.469 193.845-356.855c1.241 164.891 42.802 431.935 199.124 180.978 195.402-313.696 143.295-588.18 284.729-419.266 113.148 135.13 124.068 367.989 115.378 467.527L811.753 372.553l20.102 451.119-530.681-177.031Z" />
+      <defs>
+        <linearGradient id="10724532-9d81-43d2-bb94-866e98dd6e42" x1="1097.04" x2="-141.165" y1=".22" y2="363.075" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#776FFF" />
+          <stop offset="1" stop-color="#FF4694" />
+        </linearGradient>
+      </defs>
+    </svg>
+    <svg viewBox="0 0 1097 845" aria-hidden="true" class="absolute left-1/2 -top-52 -z-10 w-[68.5625rem] -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0">
+      <path fill="url(#8ddc7edb-8983-4cd7-bccb-79ad21097d70)" fill-opacity=".2" d="M301.174 646.641 193.541 844.786 0 546.172l301.174 100.469 193.845-356.855c1.241 164.891 42.802 431.935 199.124 180.978 195.402-313.696 143.295-588.18 284.729-419.266 113.148 135.13 124.068 367.989 115.378 467.527L811.753 372.553l20.102 451.119-530.681-177.031Z" />
+      <defs>
+        <linearGradient id="8ddc7edb-8983-4cd7-bccb-79ad21097d70" x1="1097.04" x2="-141.165" y1=".22" y2="363.075" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#776FFF" />
+          <stop offset="1" stop-color="#FF4694" />
+        </linearGradient>
+      </defs>
+    </svg>
+    <div class="absolute left-10 bottom-20 mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto max-w-2xl lg:mx-0 pb-8">
+        <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Name</h2>
+        <p class="mt-6 text-lg leading-8 text-gray-300">slogan</p>
+      </div>
+      <div class="grid grid-cols-1 gap-y-6 gap-x-8 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
+        <div>
+            <button type="submit" class="group relative flex w-full justify-center rounded-full border border-transparent bg-cyan-600 py-2 px-4 text-sm font-medium text-white hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:cyan-cyan-500 focus:ring-offset-2">
+              GET STARTED
+            </button>
+            <span class="mt-6">
+                <a href="">Already a member?</a>
+            </span>
+        </div>
+      </div>
     </div>
-</div>
-
-<div class="flex">
-    <div class="flex-none w-14 h-14">
-      01
-    </div>
-    <div class="flex-initial w-64">
-      02
-    </div>
-    <div class="flex-initial w-32">
-      03
-    </div>
-</div>
-
-<div>
-    <div>
-        <form action="" method="get">
-            <label class="relative inline-block">
-                <span class="sr-only">Search</span>
-                <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-                  <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"></svg>
-                </span>
-                <input class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search Your Location..." type="text" name="search"/>
-              </label>
-            <button class="text-white rounded-full bg-indigo-500 px-3 py-1.5 outline-transparent hover:bg-cyan-600 transition delay-100 duration-300 ease-in-out" type="submit">Search</button>
-        </form>
-    </div>
-</div>
+  </div>
 
 <style>
 
